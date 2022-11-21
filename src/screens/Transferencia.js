2d301c3aa52bf0e8852d384c042986338132cbe8
Titacele <celestereinaudo@yahoo.com.ar>
2022-11-21 19:35:59 -0300

    feat: transferencia

---
+++ src/screens/Transferencia.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {StyleSheet, View, Text, TextInput, Button, Keyboard} from "react-native"
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Constants from 'expo-constants'
+
+export default function Transferencia() {
+ 
+    return (
+        <SafeAreaView style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            }}>
+
+            <View style={{
+                position: "absolute",
+                top: 0,
+                marginTop: Constants.statusBarHeight,
+                backgroundColor: "#EBF3CE",
+                height: 100,
+                width: "100%",
+            }} />
+
+            <TitleComponent text="Transferencia" />
+            <View style={styles.title}>
+                <TextInput
+                    placeholder="CBU destino"
+                    style={styles.input}
+                    autoCapitalize="none"
+                    //deberia ser un input que te lleve al listado ya cargado de cbu?
+                />
+                <BottonComponent styleType={"btnSecndary"} text="Ingresar nuevo CBU" />
+                <TextInput
+                    placeholder="Monto"
+                    style={styles.input}
+                    autoCapitalize="none"
+                />
+                <BottonComponent styleType={"btnPrincipal"} text="Transferir" />
+            </View>
+        </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  title: {
+    width: "80%",
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  }
+});
